43981381a56fe26b598ae237ace5e6fcc6e0f73f
SamiBenAmara <samibenamara23@gmail.com>
2023-05-20 23:16:43 -0700

    Added backend and signin / signup functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import NotesProcess from './components/process/process';
 import Overlay from './components/overlay';
 import LandingPage from './pages/landing-page';
+import SignupPage from './components/SignupPage/SignupPage';
+import LoginPage from './components/LoginPage/LoginPage';
 
 function App() {
   return (
@@ -11,7 +13,8 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/notes" element={<Overlay><NotesProcess /></Overlay>} />
-          <Route path="/" element={<Overlay><LandingPage /></Overlay>} />
+          <Route path="/home" element={<Overlay><LandingPage /></Overlay>} />
+          <Route path="/" element={<><LoginPage/> <SignupPage /></>} />
         </Routes>
       </div>
     </Router>
