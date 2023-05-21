7a84a367903f1d240225b036192f018c88adb5cd
zhaobenny <benny_zhao@sfu.ca>
2023-05-20 17:08:23 -0700

    intial questions

--- src/components/process/notes.js
+++ src/components/process/notes.js
@@ -1,5 +1,108 @@
 import React, { useState } from 'react';
-
+const temptest = {
+    "questions": [
+        {
+            "question": "Which of the following is NOT part of the integumentary system?",
+            "possible_answers": [
+                "Skin",
+                "Hair",
+                "Glands",
+                "Muscles"
+            ],
+            "correct_answer": 3
+        },
+        {
+            "question": "What is the function of sebaceous glands in the integumentary system?",
+            "possible_answers": [
+                "Production of sweat",
+                "Protection against UV radiation",
+                "Production of oil for skin and hair lubrication",
+                "Secretion of cerumen in the ear canals"
+            ],
+            "correct_answer": 2
+        },
+        {
+            "question": "Which of the following is a function of the epidermis?",
+            "possible_answers": [
+                "Production of melanin",
+                "Synthesis of vitamin D",
+                "Temperature regulation",
+                "Anchoring the epidermis to the dermis"
+            ],
+            "correct_answer": 0
+        },
+        {
+            "question": "What is the main component of the dermis?",
+            "possible_answers": [
+                "Adipose tissue",
+                "Connective tissue",
+                "Muscle tissue",
+                "Nerve tissue"
+            ],
+            "correct_answer": 1
+        },
+        {
+            "question": "What is the purpose of melanin in the integumentary system?",
+            "possible_answers": [
+                "Protection against UV radiation",
+                "Production of sweat",
+                "Enhancement of tactile sensation",
+                "Production of keratin"
+            ],
+            "correct_answer": 0
+        },
+        {
+            "question": "Which layer of the skin contains the hair follicles and sweat glands?",
+            "possible_answers": [
+                "Epidermis",
+                "Dermis",
+                "Hypodermis",
+                "Stratum corneum"
+            ],
+            "correct_answer": 1
+        },
+        {
+            "question": "What is the function of eccrine sweat glands?",
+            "possible_answers": [
+                "Production of cerumen",
+                "Secretion of oil for skin lubrication",
+                "Production of hypotonic sweat for thermoregulation",
+                "Protection against UV radiation"
+            ],
+            "correct_answer": 2
+        },
+        {
+            "question": "Which type of sensory receptor is responsible for detecting temperature changes?",
+            "possible_answers": [
+                "Mechanoreceptors",
+                "Thermoreceptors",
+                "Nociceptors",
+                "Chemoreceptors"
+            ],
+            "correct_answer": 1
+        },
+        {
+            "question": "What is the role of Langerhans cells in the integumentary system?",
+            "possible_answers": [
+                "Production of melanin",
+                "Enhancement of tactile sensation",
+                "Protection against pathogens",
+                "Synthesis of vitamin D"
+            ],
+            "correct_answer": 2
+        },
+        {
+            "question": "Which layer of the epidermis contains actively dividing cells?",
+            "possible_answers": [
+                "Stratum basale",
+                "Stratum spinosum",
+                "Stratum granulosum",
+                "Stratum corneum"
+            ],
+            "correct_answer": 0
+        }
+    ]
+}
 const NotesInput = ({ onNext, data, setData }) => {
     const [text, setText] = useState('');
 
@@ -7,20 +110,58 @@ const NotesInput = ({ onNext, data, setData }) => {
         setText(event.target.value);
     };
 
+    function isJsonString(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
+
     const handleButtonClick = async () => {
-        // const { Configuration, OpenAIApi } = require("openai");
 
-        // const configuration = new Configuration({
-        //     apiKey: process.env.REACT_APP_OPENAI_API_KEY,
-        // });
-        // const openai = new OpenAIApi(configuration);
+        const { Configuration, OpenAIApi } = require("openai");
+        const configuration = new Configuration({
+            apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+        });
 
-        // const completion = await openai.createCompletion({
-        //     model: "text-davinci-003",
-        //     prompt: "Hello world",
+        const openai = new OpenAIApi(configuration);
+
+        // const completion = await openai.createChatCompletion({
+        //     model: "gpt-3.5-turbo",
+        //     messages: [
+        //         {
+        //             role: "system", content: `You are going to write a JSON full of possible exam questions with the following notes.
+
+        //         Now consider the following TypeScript Interface for the JSON schema:
+
+        //         interface MultiChoiceQuestion {
+        //             question: string;
+        //             possible_answers: string[];
+        //             correct_answer: int;
+        //         }
+
+        //         interface MultiChoiceQuestions {
+        //            questions: MultiChoiceQuestion[];
+        //         }
+
+        //         Write the basics section according to the Message schema.
+        //         On the response, include only the JSON.
+        //         `},
+        //         { role: "user", content: `Notes: ${text}` },
+
+        //     ],
         // });
-        // setData(completion.data.choices[0].text);
-        // console.log(completion.data.choices[0].text);
+        // if (!isJsonString(completion.data.choices[0].message.content)) {
+        //     alert("Not a json sadge, try again")
+        // }
+        // else {
+        //     setData(completion.data.choices[0].message.content);
+        //     onNext();
+        // }
+        //
+        setData(temptest);
         onNext();
     };
 
@@ -41,7 +182,7 @@ const NotesInput = ({ onNext, data, setData }) => {
                     onChange={handleChange} />
             </div>
             <p className="mt-3 text-sm leading-6 text-gray-600">Insert your notes to generate practice questions.</p>
-        </div><p>{text}</p>
+        </div>
             <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
                 onClick={handleButtonClick}
--- src/components/process/questions.js
+++ src/components/process/questions.js
@@ -1,12 +1,79 @@
 import React, { useState } from 'react';
 
 const TestQuestions = ({ onNext, data, setData }) => {
+
+    const handleJsonChange = (event, index, prop) => {
+        const { value } = event.target;
+
+        setData((prevJson) => {
+            const updatedQuestions = [...prevJson.questions];
+            updatedQuestions[index] = {
+                ...updatedQuestions[index],
+                [prop]: value,
+            };
+
+            return {
+                questions: updatedQuestions,
+            };
+        });
+    };
+
+    const handleClick = () => {
+        onNext();
+    };
+
     return (
         <>
-            <p> Insert questions here</p>
+            {data.questions.map((question, index) => (
+                <div key={index}>
+                    <h4>Question {index + 1}</h4>
+                    <div>
+                        <label htmlFor={`question-${index}`}>Question: </label>
+                        <input
+                            type="text"
+                            class="w-full"
+                            id={`question-${index}`}
+                            value={question.question}
+                            onChange={(event) => handleJsonChange(event, index, 'question')}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor={`possible-answers-${index}`}>Possible Answers: </label>
+                        {question.possible_answers.map((answer, answerIndex) => (
+                            <div key={answerIndex}>
+                                <input
+                                    type="text"
+                                    id={`possible-answer-${index}-${answerIndex}`}
+                                    class="w-full"
+                                    value={answer}
+                                    onChange={(event) => {
+                                        const updatedAnswers = [...question.possible_answers];
+                                        updatedAnswers[answerIndex] = event.target.value;
+                                        handleJsonChange(
+                                            { target: { value: updatedAnswers } },
+                                            index,
+                                            'possible_answers'
+                                        );
+                                    }}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                    <div>
+                        <label htmlFor={`correct-answer-${index}`}>Correct Answer: </label>
+                        <input
+                            type="number"
+                            id={`correct-answer-${index}`}
+                            class="w-full"
+                            value={question.correct_answer}
+                            onChange={(event) => handleJsonChange(event, index, 'correct_answer')}
+                        />
+                    </div>
+                </div>
+            ))}
             <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-                onClick={onNext}
+                onClick={handleClick}
             >
                 Finished?
             </button>
