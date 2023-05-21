ceaa46a4e81a5103c997ba0929f94e8fc120aa6d
MigBonkey <donghakdh@hotmail.com>
2023-05-20 22:52:59 -0700

    s

--- src/pages/landing-page.js
+++ src/pages/landing-page.js
@@ -19,14 +19,15 @@ const LandingPage = () => {
             zIndex: -1,
           }}
         />
-        <main className="container mx-auto px-4 py-12 my-8">
-          <div className="max-w-3xl mx-auto">
+        <main className="container mx-auto px-4 py-20 ">
+          <div className="max-w-3xl mx-auto mt-20">
             <h1 className="text-5xl font-bold text-gray-300 mb-30">
               Welcome to AIQ
             </h1>
-            <p className="text-gray-500 my-20">
+            <p className="text-[#7a7a7a] my-20">
               Unleash the power of your notes. Transform your learning journey
-              with intelligent flashcards and practice tests, powered by ChatGPT.
+              with the help of Artifical Intelligence.
+              <br />
               Take your knowledge to new heights, one note at a time.
             </p>
             <button className="bg-[#00adb5] hover:bg-[#00565a] text-white font-bold py-6 px-14 rounded-lg text-2xl">
