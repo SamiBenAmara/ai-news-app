222a38090e0c2ab7b0f7eb32bd4fedf413a404f2
zhaobenny <benny@zhao.gg>
2023-05-21 01:56:59 -0700

    fix highlighting

--- src/components/process/done.js
+++ src/components/process/done.js
@@ -2,8 +2,8 @@ const FinishedNotes = ({ onNext, data, setData }) => {
     console.log(data);
     return (
         <div class="text-center p-4">
-            <h1 class="text-4xl font-bold mb-4">Your questions are all done!</h1>
-            <p class="text-lg">Head <a href="/tests" class="text-blue-500">here next</a>, happy studying! 😊</p>
+            <h1 class="text-4xl font-bold mb-4">All saved.</h1>
+            <p class="text-xl">Head <a href="/tests" class="text-blue-500">here next</a>, happy studying! 😊</p>
         </div>
     );
 };
