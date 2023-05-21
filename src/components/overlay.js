41b46acbb307dcac4952b023d8c87402838482af
zhaobenny <benny@zhao.gg>
2023-05-21 00:43:46 -0700

    dddd

--- src/components/overlay.js
+++ src/components/overlay.js
@@ -9,7 +9,7 @@ const user = {
         'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
 const navigation = [
-    { name: 'Home', href: '/', current: true },
+    { name: 'Home', href: '/home', current: true },
     { name: 'Generate Questions', href: '/generate', current: false },
     { name: 'Tests', href: '/tests', current: false },
 ]
