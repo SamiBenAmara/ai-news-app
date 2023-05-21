39e67d60807166da598dbc155b98568a4e026d11
zhaobenny <benny_zhao@sfu.ca>
2023-05-20 19:18:27 -0700

    asdasd

--- src/components/process/questions.js
+++ src/components/process/questions.js
@@ -2,21 +2,28 @@ import React, { useState } from 'react';
 
 const TestQuestions = ({ onNext, data, setData }) => {
 
-    const handleJsonChange = (event, index, prop) => {
+    const handleJsonChange = (
+        event,
+        section,
+        index,
+        prop
+    ) => {
         const { value } = event.target;
 
-        setData((prevJson) => {
-            const updatedQuestions = [...prevJson.questions];
-            updatedQuestions[index] = {
-                ...updatedQuestions[index],
+        setData((data) => {
+            const updatedSection = [...data[section]];
+            updatedSection[index] = {
+                ...updatedSection[index],
                 [prop]: value,
             };
 
             return {
-                questions: updatedQuestions,
+                ...data,
+                [section]: updatedSection,
             };
         });
     };
+
 
     const handleClick = () => {
         onNext();
@@ -24,33 +31,33 @@ const TestQuestions = ({ onNext, data, setData }) => {
 
     return (
         <>
-            {data.questions.map((question, index) => (
+            <h2>Multiple Choice Questions</h2>
+            {data.multichoices.map((question, index) => (
                 <div key={index}>
                     <h4>Question {index + 1}</h4>
                     <div>
-                        <label htmlFor={`question-${index}`}>Question: </label>
+                        <label htmlFor={`mc-question-${index}`}>Question: </label>
                         <input
                             type="text"
-                            class="w-full"
-                            id={`question-${index}`}
+                            id={`mc-question-${index}`}
                             value={question.question}
-                            onChange={(event) => handleJsonChange(event, index, 'question')}
+                            onChange={(event) => handleJsonChange(event, 'multichoices', index, 'question')}
                         />
                     </div>
                     <div>
-                        <label htmlFor={`possible-answers-${index}`}>Possible Answers: </label>
+                        <label htmlFor={`mc-possible-answers-${index}`}>Possible Answers: </label>
                         {question.possible_answers.map((answer, answerIndex) => (
                             <div key={answerIndex}>
                                 <input
                                     type="text"
-                                    id={`possible-answer-${index}-${answerIndex}`}
-                                    class="w-full"
+                                    id={`mc-possible-answer-${index}-${answerIndex}`}
                                     value={answer}
                                     onChange={(event) => {
                                         const updatedAnswers = [...question.possible_answers];
                                         updatedAnswers[answerIndex] = event.target.value;
                                         handleJsonChange(
-                                            { target: { value: updatedAnswers } },
+                                            event,
+                                            'multichoices',
                                             index,
                                             'possible_answers'
                                         );
@@ -60,13 +67,71 @@ const TestQuestions = ({ onNext, data, setData }) => {
                         ))}
                     </div>
                     <div>
-                        <label htmlFor={`correct-answer-${index}`}>Correct Answer: </label>
+                        <label htmlFor={`mc-correct-answer-${index}`}>Correct Answer: </label>
                         <input
                             type="number"
-                            id={`correct-answer-${index}`}
-                            class="w-full"
+                            id={`mc-correct-answer-${index}`}
                             value={question.correct_answer}
-                            onChange={(event) => handleJsonChange(event, index, 'correct_answer')}
+                            onChange={(event) => handleJsonChange(event, 'multichoices', index, 'correct_answer')}
+                        />
+                    </div>
+                </div>
+            ))}
+
+            <h2>True/False Questions</h2>
+            {data.truefalse.map((question, index) => (
+                <div key={index}>
+                    <h4>Question {index + 1}</h4>
+                    <div>
+                        <label htmlFor={`tf-question-${index}`}>Question: </label>
+                        <input
+                            type="text"
+                            id={`tf-question-${index}`}
+                            value={question.question}
+                            onChange={(event) => handleJsonChange(event, 'truefalse', index, 'question')}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor={`tf-correct-answer-${index}`}>Correct Answer: </label>
+                        <select
+                            id={`tf-correct-answer-${index}`}
+                            value={question.correct_answer.toString()}
+                            onChange={(event) =>
+                                handleJsonChange(
+                                    event,
+                                    'truefalse',
+                                    index,
+                                    'correct_answer'
+                                )
+                            }
+                        >
+                            <option value="true">True</option>
+                            <option value="false">False</option>
+                        </select>
+                    </div>
+                </div>
+            ))}
+
+            <h2>Flashcards</h2>
+            {data.flashcards.map((flashcard, index) => (
+                <div key={index}>
+                    <h4>Flashcard {index + 1}</h4>
+                    <div>
+                        <label htmlFor={`flashcard-front-${index}`}>Front: </label>
+                        <input
+                            type="text"
+                            id={`flashcard-front-${index}`}
+                            value={flashcard.front}
+                            onChange={(event) => handleJsonChange(event, 'flashcards', index, 'front')}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor={`flashcard-back-${index}`}>Back: </label>
+                        <input
+                            type="text"
+                            id={`flashcard-back-${index}`}
+                            value={flashcard.back}
+                            onChange={(event) => handleJsonChange(event, 'flashcards', index, 'back')}
                         />
                     </div>
                 </div>
