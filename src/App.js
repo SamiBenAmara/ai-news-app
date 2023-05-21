b09fcf7438e176ed98a33245275e9dac7e3c26a1
Mentholantum <harrisontseng7@gmail.com>
2023-05-20 19:22:27 -0700

    Made some changes to second page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import NotesProcess from './Components/process/process';
-import Overlay from './Components/overlay';
+import NotesProcess from './components/process/process';
+import Overlay from './components/overlay';
 import LandingPage from './pages/landing-page';
 
 function App() {
