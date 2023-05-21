ef80b8de2b5e33a85f7627ec4189eeaed3081fc5
zhaobenny <benny@zhao.gg>
2023-05-21 00:30:30 -0700

    small changes

--- src/components/process/done.js
+++ src/components/process/done.js
@@ -1,9 +1,10 @@
 const FinishedNotes = ({ onNext, data, setData }) => {
     console.log(data);
     return (
-        <>
-            <h1>All done lol</h1>
-        </>
+        <div class="text-center p-4">
+            <h1 class="text-4xl font-bold mb-4">Your questions are all done!</h1>
+            <p class="text-lg">Head <a href="/tests" class="text-blue-500">here next</a>, happy studying! 😊</p>
+        </div>
     );
 };
 
